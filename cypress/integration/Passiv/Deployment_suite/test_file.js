025d87572b0db54e6f0c85da89e3206b6f11daa1
Alex Sutheralnd <alex.sutherland@getpassiv.com>
2021-01-25 13:06:40 -0400

    Created stubbing tests for all XHR calls

--- cypress/integration/Passiv/Deployment_suite/test_file.js
+++ cypress/integration/Passiv/Deployment_suite/test_file.js
@@ -1,290 +1,101 @@
-describe('Desktop goal setting test', () => {
+describe('Login using created data from registration', () => {
+    it('Log in Success', () => {
 
-    it('Login test 2', () => {
-        // redirect for any other domains
-        
-        cy.fixture('testDomain').as('login')
-        cy.get('@login').then(domain => {
-        cy.visit((domain.test).concat('/login')) })
-        cy.fixture('credentials').as('userFixture')
-        cy.get('@userFixture').then(user => {
-        cy.get('[name=email]').type(user.username)
-        cy.get('[placeholder=Password]').type(user.password)
-        })
+      
+      const v1 = { fixture: 'login_stubs/v1'}
+      const ping = { fixture: 'login_stubs/ping'}
+      const auth = { fixture: 'login_stubs/autho'}
+      const accounts = { fixture: 'login_stubs/accounts'}
+      const balances = { fixture: 'login_stubs/balances'}
+      const brokerages = { fixture: 'login_stubs/brokerages'}
+      const currencies = { fixture: 'login_stubs/currencies'}
+      const features = { fixture: 'login_stubs/features'}
+      const goals = { fixture: 'login_stubs/goals'}
+      const help = { fixture: 'login_stubs/help'}
+      const incentives = { fixture: 'login_stubs/incentives'}
+      const info = { fixture: 'login_stubs/info'}
+      const performance = { fixture: 'login_stubs/performance'}
+      const plans = { fixture: 'login_stubs/plans'}
+      const portfolioGroups = { fixture: 'login_stubs/portfolioGroups'}
+      const positions = { fixture: 'login_stubs/positions'}
+      const settings = { fixture: 'login_stubs/settings'}
+      const subscriptions = { fixture: 'login_stubs/subscriptions'}
 
-    cy.get('[data-cy=login-button]').then(($button) => {
-        if (cy.get($button).click().should('be.disabled') ){
-            cy.fixture('testDomain').as('server')
-            cy.get('@server').then(domain => {
-            cy.get('div').contains('Sign Up').click()})
-            cy.fixture('credentials').as('userFixture')
-                cy.get('@userFixture').then(user => {
-                cy.get('[name=name').type('Al')
-                cy.get('[name=email]').first().type(user.username)
-                cy.get('[placeholder=Password]').type(user.password)})
+      cy.intercept('/api/v1/ping', (req) => { req.reply({interceptor: ping}) })
+      .as('ping')
 
-    } else {
+      cy.intercept('/api/v1/incentives/', (req) => { req.reply({interceptor: incentives}) })
+      .as('incentives')
 
-        cy.fixture('testDomain').as('login')
-        cy.get('@login').then(domain => {
-        cy.visit((domain.test).concat('/register')) })
-        cy.fixture('credentials').as('userFixture')
-        cy.get('@userFixture').then(user => {
-        cy.get('[name=email]').type(user.username)
-        cy.get('[placeholder=Password]').type(user.password)})
+      cy.intercept('/api/v1/features/', (req) => { req.reply({interceptor: features}) })
+      .as('features')
 
-    }
-})
+      cy.intercept('/api/v1/currencies/rates/', (req) => { req.reply({interceptor: currencies}) })
+      .as('currencies')
 
-        it('Goals Test', () => {
-            cy.contains('Goals').click()
-            .should('have.attr', 'href', '/app/goals')
+      cy.intercept('/api/v1/subscriptions/', (req) => { req.reply({interceptor: subscriptions}) })
+      .as('subs')
 
+      cy.intercept('/api/v1/accounts/', (req) => { req.reply({interceptor: accounts}) })
+      .as('accounts')
+
+      cy.intercept('/api/v1/portfolioGroups/', (req) => { req.reply({interceptor: portfolioGroups}) })
+      .as('portfolioGroups')
+
+      cy.intercept('/api/v1/settings/', (req) => { req.reply({interceptor: settings}) })
+      .as('settings')
+
+      cy.intercept('/api/v1/plans/', (req) => { req.reply({interceptor: plans}) })
+      .as('plans')
+
+      cy.intercept('/api/v1/goals/', (req) => { req.reply({interceptor: goals}) })
+      .as('goals')
+
+      cy.intercept('/api/v1/help/', (req) => { req.reply({interceptor: help}) })
+      .as('help')
+
+      cy.intercept('/api/v1/brokerages/', (req) => { req.reply({interceptor: brokerages}) })
+      .as('brokerages')
+
+      cy.intercept('/api/v1/authorizations', (req) => { req.reply({interceptor: auth}) })
+      .as('auth')
+
+      cy.intercept('/api/v1/performance/all/**', (req) => { req.reply({interceptor: performance}) })
+      .as('performance')
+
+      cy.intercept('/api/v1/accounts/**/balances/', (req) => { req.reply({interceptor: balances}) })
+      .as('balances')
+
+      cy.intercept('/api/v1/accounts/**/positions/', (req) => { req.reply({interceptor: positions}) })
+      .as('positions')
+      
+      cy.intercept('/api/v1/portfolioGroups/**/info/', (req) => { req.reply({interceptor: info}) })
+      .as('info')
+      
+      cy.intercept('v1', (req) => { req.reply({interceptor: v1}) })
+      .as('v1')
+
+
+      cy.fixture('testDomain').as('login')
+      cy.get('@login').then(domain => {
+      cy.visit((domain.test).concat('/login')) })
+      cy.location('pathname').should('equal', '/app/login')
+
+    // enter valid username and password
+    cy.fixture('credentials').as('userFixture')
+    cy.get('@userFixture').then(user => {
+    cy.get('[name=email]').first().type(user.username)
+    cy.get('[placeholder=Password]').type(user.password)
+    .get('form').submit()
+
+    cy.intercept('POST','/api/v1/auth/login', (req) => { req.reply({interceptor: v1}) })
+    .as('v1')
+
+    
+ 
+      })
 
     })
-
-})
-
-})
+  })
 
 
-//     //these are the values for the goal
-//     const goal1 = "Get the bag"
-//     const goalnumber = "1000000"
-//     const month = "July"
-//     const year = "2050"
-
-//     const goal2 = "Get the bread"
-//     const goalnumber2 = "10000000"
-
-
-
-//     it('Create a goal name', () => {
-//         cy.get('[id=goalname]')
-//         .clear()
-//         .type(goal1)
-//         .should('have.value', goal1)
-//     })
-
-//     it('Next' , () => {
-//         cy.get('div').find('button').contains('Next')
-//         .click()
-//     })
-
-
-//     it('Optional Account Selection ' , () => {
-//         cy.get('div').find('button').contains('All Accounts')
-//         .click()
-//     })
-
-// //     // This is the block for no account@class='css-jm466k']
-
-//     // it('Pick portfolio Account ' , () => {
-//     //     cy.get('div').find('button').contains('Retirement TFSA')
-//     //     .click()
-//     // })
-
-//     it('Next' , () => {
-//         cy.get('div').find('button').contains('Next')
-//         .click()
-//     })
-
-//     it('Enter goal ammount', () => {
-//         cy.get('div').find('label').contains('I want to reach $').next()   
-//         .click({multiple:true})
-//         .type(goalnumber)
-//         .should('have.value', goalnumber)
-//     })
-
-//     it('Enter Year', () => {
-//         cy.get('div').find('label').contains('By').next().next()
-//         .clear()
-//         .type(year)
-//         .should('have.value', year)
-//     })
-
-//     it('Confirm Goal', () => {
-//         cy.get('button').contains('Start Saving!').click()
-//         cy.get('button').contains('Refresh').click().wait(4000)
-
-//     })
-
-//     it('Return to Dashboard  Page', () => {
-//         cy.fixture('testDomain').as('login')
-        
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/dashboard')) })
-//         cy.get('button').contains('Refresh').click().wait(4000)
-//     })
-
-
-//     it('Return to Goals Page', () => {
-//         cy.fixture('testDomain').as('login')
-        
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/goals')) })
-//         cy.get('button').contains('Refresh').click()
-//     })
-
-
-//     it('Edit Goal', () => {
-//         cy.contains('Goals').click()
-//         cy.get('div').find('h2').contains(goal1)
-//         .click({multiple:true})
-//     })
-
-//     it('Change goal name', () => {
-//         cy.get('div').find('div.css-ov1ktg main.css-ozbw39 div.css-875kku div.css-2lma4n div:nth-child(3) > button.css-1v6e5e8').click() 
-//         cy.get('button').contains('Edit Name').click({multiple: true})
-//         .get('div').find('input').first()
-//         .clear()
-//         .type(goal2)
-
-//         cy.contains('Finish').click()
-//     })
-
-//     it('Update Goal', () => {
-//         cy.get('button').contains('Update Goal').click()
-
-//     })
-
-//     it('Update the target amount', () => {
-//         cy.get('button').contains('Edit Target').click()
-//         .get('div').find('input').last()
-//         .clear()
-//         .type(goalnumber2)
-//         .get('button').contains('Update').click()
-
-//     })
-
-//     it('Update Goal', () => {
-//         cy.get('button').contains('Update Goal').click()
-
-//     })
-
-//     it('Return to Dashboard  Page', () => {
-//         cy.fixture('testDomain').as('login')
-        
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/dashboard')) })
-//         cy.get('button').contains('Refresh').click()
-//     })
-
-//     it('Return to Goals Page and add 2nd goal', () => {
-//         cy.fixture('testDomain').as('login')
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/goals')) })
-//         cy.get('button').contains('Refresh').click()
-//         .get('button').contains('Add Goal').click()
-
-//     })
-
-//     // This is where  the 2nd goal is added to confirm it iterates correctly if the same name is entered
-
-
-//     it('Create a goal name', () => {
-//         cy.get('[id=goalname]')
-//         .clear()
-//         .type(goal1)
-//         .should('have.value', goal1)
-//     })
-
-//     it('Optional Account Selection ' , () => {
-//         cy.get('div').find('button').contains('Next')
-//         .click()
-//     })
-
-//     // This is the block for no account
-
-//     it('Pick no account' , () => {
-//         cy.get('div').find('button').contains('Next')
-//         .click()
-//     })
-
-//     // it('Pick portfolio Account ' , () => {
-//     //     cy.get('div').find('button').contains('Retirement TFSA')
-//     //     .click()
-//     // })
-
-
-//     it('Enter goal ammount', () => {
-//         cy.get('div').find('label').contains('I want to reach $').next()   
-//         .click({multiple:true})
-//         .type(goalnumber)
-//         .should('have.value', goalnumber)
-//     })
-
-
-//     it('Enter Year', () => {
-//         cy.get('div').find('label').contains('By').next().next()
-//         .clear()
-//         .type(year)
-//         .should('have.value', year)
-//     })
-
-//     it('Confirm Goal', () => {
-//         cy.get('button').contains('Start Saving!').click()
-//     })
-
-//     it('Reset to Dashboard', () => {
-//         cy.fixture('testDomain').as('login')      
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/Dashboard')) })
-//     })
-
-//     it('View all Goals', () => {
-//             cy.fixture('testDomain').as('login')
-        
-//             cy.get('@login').then(domain => {
-//             cy.visit((domain.test).concat('/goals')) })
-//     })
-
-// describe('Delete goals', () => {
-//     it('Delete the goal2', () => {
-//         cy.fixture('testDomain').as('login')
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/goals')) })
-//         cy.get('div').find('h2').contains(goal2).click() 
-//         cy.get('div').find('div.css-ov1ktg main.css-ozbw39 div.css-875kku div.css-2lma4n div:nth-child(3) > button.css-1v6e5e8').click()
-//         cy.contains('Delete').click({multiple: true})
-//         cy.get('button').find('button.css-jm466k').should('be.visible').wait(8000).contains('Delete').click()
-//         cy.get('button').contains('Refresh').click()
-
-
-//     })
-
-//     it('Reset to Dashboard', () => {
-//         cy.fixture('testDomain').as('login')      
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/Dashboard')) })
-//         cy.get('button').contains('Refresh').click()
-//     })
-
-//     it('Delete the first goal', () => {
-//         cy.fixture('testDomain').as('login')
-
-//         cy.get('@login').then(domain => {
-//         cy.visit((domain.test).concat('/goals')) })
-//         cy.get('div').find('h2').contains(goal1).click() 
-//         cy.get('div').find('div.css-ov1ktg main.css-ozbw39 div.css-875kku div.css-2lma4n div:nth-child(3) > button.css-1v6e5e8').click()
-//         cy.contains('Delete').click({multiple: true})
-//         cy.get('.aria-modal').should('be.visible')
-//         .wait(8000).contains('Delete').click()
-//         cy.get('button').contains('Refresh').click()
-
-//     })
-
-
-//             // .get('div').find('input').first()
-//             // .clear()
-//             // .type(goal2)  
-        
-
-//     it('Logout', () => {
-//     cy.get('button').contains('Logout').click()
-
-//     })
-//     })
-// })
-
-//     }
